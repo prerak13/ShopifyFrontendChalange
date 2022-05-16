f5af4e81ea89878c8b472ee402f27919bc536fe7
Prerak Choksi <prerakchoksi@TF278.WPA.Dal.Ca>
2022-05-16 18:54:22 -0300

    added env congif

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import { useState } from "react";
 import InputForm from "./Components/InputForm";
 import Response from "./Components/Response";
-require("dotenv").config();
 
 function App() {
   const apiKey = process.env.API_KEY;
