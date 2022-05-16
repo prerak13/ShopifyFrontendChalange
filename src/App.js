4798168fbe8c6bbe9e448d6acd281c1e580a2826
Prerak Choksi <prerakchoksi@TF278.WPA.Dal.Ca>
2022-05-16 19:36:30 -0300

    added loader

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import axios from "axios";
 import { useState } from "react";
 import InputForm from "./Components/InputForm";
 import Response from "./Components/Response";
+import { Card, CardBody, CardTitle } from "reactstrap";
 
 function App() {
-  const apiKey = process.env.REACT_APP_API_KEY;
+  const apiKey =
+    process.env.REACT_APP_API_KEY ||
+    "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";
   const [responses, setResponses] = useState(
     JSON.parse(localStorage.getItem("queries")) || []
   );
   const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
   const handleQueryChange = (e) => {
     setQuery(e.target.value);
   };
@@ -33,13 +37,13 @@ function App() {
       },
     };
 
+    setLoading(true);
     const { data } = await axios.post(
       `https://api.openai.com/v1/engines/text-curie-001/completions`,
-
       postData,
       axiosConfig
     );
-
+    setLoading(false);
     localStorage.setItem(
       "queries",
       JSON.stringify([{ query, response: data.choices[0].text }, ...responses])
@@ -59,6 +63,13 @@ function App() {
       <div className="row">
         <div className="col-9  offset-md-2">
           {responses.length > 0 && <h3> Responses:</h3>}
+          {loading && (
+            <Card>
+              <div class="spinner-border offset-md-6" role="status">
+                <span class="sr-only"></span>
+              </div>
+            </Card>
+          )}
           {responses.map((response, index) => {
             return (
               <Response
